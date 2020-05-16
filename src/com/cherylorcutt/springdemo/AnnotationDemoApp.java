a8dc8fc14730f0c1462a3c169120af2e6d2e2358
daisymae <daisymae.orcadiafarm@gmail.com>
2020-05-16 17:34:34 -0500

    Value annotations
    Adding @Value annotations for properties from a properties file.

--- src/com/cherylorcutt/springdemo/AnnotationDemoApp.java
+++ src/com/cherylorcutt/springdemo/AnnotationDemoApp.java
@@ -11,13 +11,19 @@ public class AnnotationDemoApp {
 		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		Coach pingCoach = context.getBean("pingPongCoach", Coach.class);
+		CricketCoach cricketCoach = context.getBean("cricketCoach", CricketCoach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		System.out.println(pingCoach.getDailyWorkout());
 		System.out.println(pingCoach.getDailyFortune());
-		
+
+		System.out.println(cricketCoach.getDailyWorkout());
+		System.out.println(cricketCoach.getDailyFortune());
+		System.out.println(cricketCoach.getTeam());
+		System.out.println(cricketCoach.getEmailAddress());
+
 		// close the context
 		context.close();
 	}
