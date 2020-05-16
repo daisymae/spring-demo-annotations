8d1a59220f5ce021ac80ea2d25a4c8752e2f57ad
daisymae <daisymae.orcadiafarm@gmail.com>
2020-05-16 15:52:58 -0500

    Basic Annotation
    Simple annotation example

---
+++ src/com/cherylorcutt/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.cherylorcutt.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+}
---
+++ src/com/cherylorcutt/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.cherylorcutt.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice backhand volley";
+	}
+
+}
