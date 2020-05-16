e687600087e44541edf371c878be74d677209431
daisymae <daisymae.orcadiafarm@gmail.com>
2020-05-16 17:04:12 -0500

    More services
    ERROR: expected single matching bean but found 4: databaseFortuneService,happyFortuneService,RESTFortuneService,randomFortuneService

---
+++ src/com/cherylorcutt/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,14 @@
+package com.cherylorcutt.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
